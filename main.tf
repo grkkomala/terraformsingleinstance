1b3e769f91d39e96cecdd015c3785b080790e3a2
grkkomala <63488991+grkkomala@users.noreply.github.com>
2020-06-21 17:04:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 #This Terraform Code Deploys Basic VPC Infra
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
